858bbfcb8820e9d770b65b5e4e72101571a5843f
daniel <daniel.neveux@gmail.com>
2017-09-14 11:11:18 +0200

    allow static properties

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,37 @@
-import Vue, { ComponentOptions } from 'vue'
-import { VueClass } from './declarations'
-import { componentFactory, $internalHooks } from './component'
+import Vue, { ComponentOptions } from "vue";
+import { VueClass } from "./declarations";
+import { componentFactory, $internalHooks } from "./component";
 
-export { createDecorator } from './util'
+export { createDecorator } from "./util";
 
-function Component <U extends Vue>(options: ComponentOptions<U>): <V extends VueClass>(target: V) => V
-function Component <V extends VueClass>(target: V): V
+function Component <U extends Vue>(options: ComponentOptions<U>): <V extends VueClass>(target: V) => V;
+function Component <V extends VueClass>(target: V): V;
 function Component <V extends VueClass, U extends Vue>(
- options: ComponentOptions<U> | V
+    options: ComponentOptions<U> | V
 ): any {
-  if (typeof options === 'function') {
-    return componentFactory(options)
+  if (typeof options === "function") {
+    return componentFactory(options);
   }
-  return function (Component: V) {
-    return componentFactory(Component, options)
-  }
+  return function (Comp: V) {
+
+    // Collect static properties
+    const staticProps: Array<{key: string, value: any}> = Object.keys(Comp)
+        .filter(key => key.charAt(0) !== "_")
+        .map(key => ({key, value: Comp[key]}));
+
+    const Component = componentFactory(Comp, options);
+
+    // Re add static properties
+    staticProps.forEach(prop => Component[prop.key] = prop.value);
+
+    return Component;
+  };
 }
 
 namespace Component {
   export function registerHooks (keys: string[]): void {
-    $internalHooks.push(...keys)
+    $internalHooks.push(...keys);
   }
 }
 
-export default Component
+export default Component;
